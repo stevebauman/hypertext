3aabb6a16842680e4777a83f50e5b7cca7cccd3b
Steve Bauman <steven_bauman@outlook.com>
2023-10-21 23:29:40 -0400

    Add tests

--- tests/Unit/ExampleTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Stevebauman\Hypertext\Hyper;
-
-it('foo', function () {
-    $html = file_get_contents(__DIR__.'/../stubs/email.txt');
-
-    ray(Hyper::toText($html));
-});
---
+++ tests/Unit/TransformerTest.php
@@ -0,0 +1,61 @@
+<?php
+
+use Stevebauman\Hypertext\Transformer;
+
+function fixture(string $path): string
+{
+    return __DIR__ . "/../Fixtures/$path";
+}
+
+it('transforms html to text', function (string $inputFile, string $outputFile, Closure $callback = null) {
+    $input = file_get_contents(fixture($inputFile));
+    $output = file_get_contents(fixture($outputFile));
+
+    $transformer = new Transformer();
+
+    if ($callback) {
+        $callback($transformer);
+    }
+
+    expect($transformer->toText($input))->toEqual($output);
+})->with([
+    [
+        'email/input.txt',
+        'email/output.txt',
+    ],
+    [
+        'email/input.txt',
+        'email/output-links.txt',
+        fn (Transformer $transformer) => $transformer->keepLinks(),
+    ],
+    [
+        'email/input.txt',
+        'email/output-lines.txt',
+        fn (Transformer $transformer) => $transformer->keepNewLines(),
+    ],
+    [
+        'email/input.txt',
+        'email/output-both.txt',
+        fn (Transformer $transformer) => $transformer->keepLinks()->keepNewLines(),
+    ],
+
+    [
+        'website/input.txt',
+        'website/output.txt',
+    ],
+    [
+        'website/input.txt',
+        'website/output-links.txt',
+        fn (Transformer $transformer) => $transformer->keepLinks(),
+    ],
+    [
+        'website/input.txt',
+        'website/output-lines.txt',
+        fn (Transformer $transformer) => $transformer->keepNewLines(),
+    ],
+    [
+        'website/input.txt',
+        'website/output-both.txt',
+        fn (Transformer $transformer) => $transformer->keepLinks()->keepNewLines(),
+    ],
+]);
